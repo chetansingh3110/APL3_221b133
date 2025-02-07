3daa68bce3273e7554f1a5e34deed537c9bc5251
DevanshRaghuwanshi <devansh98261@gmail.com>
2025-02-07 09:45:21 +0530

    added files

---
+++ LE03/Lab3Q1/Bheem.java
@@ -0,0 +1,11 @@
+class Bheem extends Pandav {
+    @Override
+    public void obey() {
+        System.out.println("Bheem is obedient like Arjun.");
+    }
+
+    @Override
+    public void kind() {
+        System.out.println("Bheem is less kind but still has a sense of kindness.");
+    }
+}
---
+++ LE03/Lab3Q1/Kaurav.java
@@ -0,0 +1,11 @@
+class Kaurav extends Bharatvanshi {
+    @Override
+    public void obey() {
+        System.out.println("Disobeying and creating chaos.");
+    }
+
+    @Override
+    public void kind() {
+        System.out.println("Being cruel to others.");
+    }
+}
